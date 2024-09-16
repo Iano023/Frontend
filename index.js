a0a9fefd3df9ae46271e7a38c893bf313cc30d94
Iano023 <ianalbert272@gmail.com>
2024-09-16 14:10:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ function sendNotification(id) {
     
             const notificationData = {
                 id: user.id,
-                title: 'Report Notice',
+                title: 'Report Notice: ',
                 body: `Report Submitted to Municipal Office Plate Number ${user.Plate_Number}.`,
                 serverTime: serverTime,
                 serverDate: serverDate
