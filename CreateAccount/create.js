f0cf986ee17a97231a975c332f82eeacf4b1def5
Iano023 <ianalbert272@gmail.com>
2024-10-13 14:24:11 +0800

    Add files via upload

---
+++ CreateAccount/create.js
@@ -0,0 +1,28 @@
+document.getElementById('create-account-form').addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const username = document.getElementById('register-username').value;
+    const fullname = document.getElementById('register-fullname').value;
+    const password = document.getElementById('register-password').value;
+
+    try {
+        const response = await fetch('http://localhost:4500/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, fullname, password })
+        });
+
+        const data = await response.json();
+        const messageElem = document.getElementById('register-message');
+
+        if (response.ok) {
+            messageElem.style.color = 'green';
+            messageElem.innerText = 'Account created successfully!';
+        } else {
+            messageElem.innerText = data.message || 'Error creating account';
+        }
+    } catch (error) {
+        document.getElementById('register-message').innerText = 'Error connecting to the server';
+    }
+});
