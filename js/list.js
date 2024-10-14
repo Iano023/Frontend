bceae33d8dd5aabb2e16cac55e2a49746a166184
Iano023 <ianalbert272@gmail.com>
2024-10-14 15:50:15 +0800

    Update list.js

--- js/list.js
+++ js/list.js
@@ -103,7 +103,7 @@ function displayUsers(data) {
 
         html += `
             <tr>
-                <td>${element.id}.</td>
+                <td><strong>${element.id}.</strong></td>
                 <td><img src="${imageSrc}" alt="Image" style="max-width: 100px; height: auto;" /></td>
                 <td>${element.Driver_name}</td>
                 <td>${element.Plate_number}</td>
