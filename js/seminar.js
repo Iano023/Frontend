1115cf20fb5a098c0eb5821c28fb691674b3511f
Iano023 <ianalbert272@gmail.com>
2024-10-14 19:46:32 +0800

    Add files via upload

---
+++ js/seminar.js
@@ -0,0 +1,41 @@
+function displayAdminName() {
+    const fullname = localStorage.getItem('fullname'); // Retrieve fullname from localStorage
+    const adminNameElement = document.getElementById('adminName');
+
+    if (fullname) {
+        adminNameElement.textContent = fullname; // Display fullname
+    } else {
+        adminNameElement.textContent = 'Admin'; // Default fallback
+    }
+}
+
+window.addEventListener('load', () => {
+    displayAdminName();  
+    
+});
+
+
+
+
+function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    const mainContent = document.querySelector('.main-content');
+    const sidebarOptions = document.getElementById('sidebar-options');
+
+    // Toggle the 'expanded' class on the sidebar
+    if (sidebar.classList.contains('expanded')) {
+        sidebar.classList.remove('expanded');
+        mainContent.classList.remove('shifted');
+    } else {
+        sidebar.classList.add('expanded');
+        mainContent.classList.add('shifted');
+    }
+
+     if (sidebarOptions.classList.contains('open')) {
+        sidebarOptions.classList.remove('open');
+        sidebarOptions.classList.add('hidden');
+    } else {
+        sidebarOptions.classList.add('open');
+        sidebarOptions.classList.remove('hidden');
+    }
+}
